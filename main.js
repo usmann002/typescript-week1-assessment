63b6012b82cab02e27850283ee8484b889bdafe3
usmann002 <raanausman0202@gmail.com>
2025-04-21 15:47:06 +0500

    update

--- main.js
+++ main.js
@@ -1,1 +1,50 @@
-console.log("hello world");
+// // console.log("hello world");
+// //creating the employee interface
+// interface Employee  {
+// id:number,
+// Ename:string,
+// role:string,
+// intern:boolean
+// }
+var employees = [
+    {
+        id: 1,
+        name: "usman",
+        role: "developer",
+        isIntern: true,
+    },
+    {
+        id: 2,
+        name: "salman",
+        role: "designer",
+        isIntern: false,
+    },
+    {
+        id: 3,
+        name: "haiparbat",
+        role: "manager",
+        isIntern: true,
+    },
+];
+function logIntern(isEmployee) {
+    for (var _i = 0, isEmployee_1 = isEmployee; _i < isEmployee_1.length; _i++) {
+        var employe = isEmployee_1[_i];
+        if (employe.isIntern) {
+            console.log("".concat(employe.name, " and role ").concat(employe.role));
+        }
+    }
+}
+logIntern(employees);
+//enum
+var projectStatus;
+(function (projectStatus) {
+    projectStatus["Pending"] = "isPending";
+    projectStatus["Inprogress"] = "inProgress";
+    projectStatus["completed"] = "Completed";
+})(projectStatus || (projectStatus = {}));
+var stringProject = {
+    name: "E-commerce",
+    status: projectStatus.Inprogress,
+    data: ["react", "node", "php"],
+};
+console.log(stringProject);
--- main.ts
+++ main.ts
@@ -1,55 +1,111 @@
-// console.log("hello world");
-//creating the employee interface
-interface Employee  {
-id:number,
-Ename:string,
-role:string,
-intern:boolean
-}
+// // console.log("hello world");
+// //creating the employee interface
+// interface Employee  {
+// id:number,
+// Ename:string,
+// role:string,
+// intern:boolean
+// }
 
-const employees : Employee=[
+// const employees : Employee=[
+//     {
+//         id:1,
+//         Ename:"usman",
+//         role:"developer",
+//         intern:true,
+//     },
+//     {
+//         id:2,
+//         Ename:"salman",
+//         role:"designer",
+//         intern:false
+//     },
+//     {
+//         id:3,
+//         Ename:"kashmir",
+//         role:"manager",
+//         intern:true
+//     }
+
+// ];
+
+// function empDetail(employees[]):Employee{
+// if(employees.intern == true){
+//     console.log(employees.name);
+//     console.log(employees.role);
+// }
+// }
+
+// //enum
+// enum projectStatus {"pending","InProgress", "completed" };
+
+// //
+// interface Project<T>{
+//     name:string,
+//     status:boolean,
+//     data:T;
+// }
+
+// const myArr:Project<string []> [
+//     technologies:[
+
+//     ]
+// ]
+
+
+
+interface Employee {
+    id:number,
+    name:string,
+    role:"developer" | "designer" | "manager",
+    isIntern: boolean
+}
+const employees:Employee[]=[
     {
         id:1,
-        Ename:"usman",
+        name:"usman",
         role:"developer",
-        intern:true,
+        isIntern:true,
     },
     {
         id:2,
-        Ename:"salman",
+        name:"salman",
         role:"designer",
-        intern:false
+        isIntern:false,
     },
     {
         id:3,
-        Ename:"kashmir",
+        name:"haiparbat",
         role:"manager",
-        intern:true
-    }
-
+        isIntern:true,
+    },
 ];
-
-function empDetail(employees[]):Employee{
-if(employees.intern == true){
-    console.log(employees.name);
-    console.log(employees.role);
+function logIntern(isEmployee:Employee[]):void{
+    for(const employe of isEmployee){
+        if(employe.isIntern){
+            console.log(`${employe.name} and role ${employe.role}`);
+        }
+    }
 }
-}
+logIntern(employees);
 
 //enum
-enum projectStatus {"pending","InProgress", "completed" };
+enum projectStatus{
+Pending= "isPending",
+Inprogress = "inProgress",
+completed = "Completed",
 
-//
-interface Project<T>{
-    name:string,
-    status:boolean,
-    data:T;
 }
 
-const myArr:Project<string []> [
-    technologies:[
-
-    ]
-]
-
-
+//generic interface
+interface Project<T>{
+    name:string,
+    status:projectStatus,
+    data:T,
+}
+const stringProject:Project<string[]> = {
+    name:"E-commerce",
+    status:projectStatus.Inprogress,
+    data: ["react", "node", "php"],
+};
+console.log(stringProject);
